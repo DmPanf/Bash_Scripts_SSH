a21a8d09705ee65d546399a945d3f1c57c1b7cb1
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-09-05 01:34:50 +0300

    Create config.sh

---
+++ Port_Forwarding/config.sh
@@ -0,0 +1,8 @@
+# config.sh
+# 📆 [Created]: 03-09-2023
+# 🔋 [Last Modified]: 05-09-2023
+# 💾 [File]: /home/${USER}/scr/config.sh
+# 📠 [Description]: Private Data
+# 👤 [Author]: Dmitrii [https://github.com/dmpanf]
+SSH_PORT=xxxxx
+SSH_KEY="id_ed25519.XXX"
